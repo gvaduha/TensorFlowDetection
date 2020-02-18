0c8892d427950d6954ced687dc87944f6191e178
gvaduha <gvaduha@gmail.com>
2020-02-18 12:01:38 +0700

    Restructure

--- SharpStresser/Settings.Designer.cs
+++ SharpStresser/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace TensorSharpStresser {
+namespace SharpStresser {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
